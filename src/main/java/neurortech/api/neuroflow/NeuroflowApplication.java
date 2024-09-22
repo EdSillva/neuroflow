bb4b7d64ec995179fd90b6e35d6ac5b9e589079c
EdSilva23 <jose.edson@jcpm.com.br>
2024-09-22 12:39:58 -0300

    create new project

---
+++ src/main/java/neurortech/api/neuroflow/NeuroflowApplication.java
@@ -0,0 +1,13 @@
+package neurortech.api.neuroflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuroflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/neurortech/api/neuroflow/NeuroflowApplicationTests.java
@@ -0,0 +1,12 @@
+package neurortech.api.neuroflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
